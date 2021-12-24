3f734c9cd1024e9c5e6c75c72bcc6c358a4c4632
blush-old <779145208@qq.com>
2021-12-24 19:26:03 +0800

    add mitt

--- geek-vue/vue-component/src/router/index.ts
+++ geek-vue/vue-component/src/router/index.ts
@@ -1,23 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import View1 from "../views/View1.vue";
+import View2 from "../views/View2.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "view1",
+      component: View1,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/view2",
+      name: "view2",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: View2,
+    },
+  ],
+});
 
-export default router
+export default router;
